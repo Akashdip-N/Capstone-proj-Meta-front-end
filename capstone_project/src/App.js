70d4b1e37e40a54a9b78f9d700132f7f773f186a
Akashdip Neogi <Akashdipneogi@gmail.com>
2025-05-28 23:19:50 +0100

    Project Creation and some changes

---
+++ capstone_project/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Nav />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ capstone_project/src/Header.js
@@ -0,0 +1,8 @@
+function Header() {
+
+    return (
+        <img src="../images/little_lemon.png" alt="Little Lemon Logo" className="nav-logo" />
+    );
+}
+
+export default Header;
